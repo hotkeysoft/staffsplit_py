bfb3f31af038967f2e8073e3abcff892cda06654
hotkeysoft <dev@hotkeysoft.net>
2021-05-29 13:22:08 -0400

    basic arguments

--- staffsplit/args.py
+++ staffsplit/args.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import argparse
 import logging
+import os
+from posixpath import normpath
 import pkg_resources
 log = logging.getLogger(__name__)
+
 
 def parse_args(args=None):
     parser = argparse.ArgumentParser(
@@ -10,11 +13,29 @@ def parse_args(args=None):
         fromfile_prefix_chars='@'
     )
 
-    action_group = parser.add_mutually_exclusive_group(required=True)
-    action_group.add_argument(
+    parser.add_argument(
+        'infile',
+        help="input image file(s)",
+        nargs='*',
+    )
+
+    parser.add_argument(
         '--version',
         help="print version info",
         action="store_true"
-    )    
+    )
+
+    debug_group = parser.add_mutually_exclusive_group()
+    debug_group.add_argument(
+        '--debug',
+        help="print debugging statements",
+        action="store_const", dest="loglevel", const=logging.DEBUG,
+        default=logging.WARNING,
+    )
+    debug_group.add_argument(
+        '-v', '--verbose',
+        help="print additional information",
+        action="store_const", dest="loglevel", const=logging.INFO,
+    )
 
     return parser.parse_args(args)
